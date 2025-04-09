a51bec7d155cc882b80ecf042c931df797b965d1
plinecom <plinecom@gmail.com>
2025-04-09 12:03:24 +0900

    Add EXR to ACEScg color space conversion script
    This commit introduces a Python script to convert EXR files from ACES2065-1 to ACEScg color space using OpenImageIO. It also includes the corresponding OCIO configuration file required for accurate color transformation.

---
+++ main.py
@@ -0,0 +1,24 @@
+import OpenImageIO as oiio
+import glob
+import os
+
+if __name__ == '__main__':
+
+    # 対象のディレクトリを指定
+
+    for path in glob.glob('C:/EXR/ACES2065_1/*.exr'):
+        new_folder = os.path.dirname(path) + '/ACEScg'
+        os.makedirs(new_folder, exist_ok=True)
+        new_path = new_folder +'/'+ os.path.basename(path)
+
+        print(new_path)
+
+        #Read File
+        buf = oiio.ImageBuf(str(path))
+
+        #Convert ColorSpace
+        dst = oiio.ImageBufAlgo.colorconvert( buf, "ACES2065-1", "ACEScg",
+                                      colorconfig='studio-config-v2.2.0_aces-v1.3_ocio-v2.4.ocio')
+
+        #Write File
+        dst.write(str(new_path))
